70926602555901a097584be7e43c4416796c98d5
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-09-16 08:06:25 -0500

    updated styling for success component.

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,13 +1,42 @@
 import { useHistory } from 'react-router-dom';
 
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import CreateIcon from '@mui/icons-material/Create';
+
 const Success = () => {
     // use history to navigate between pages
     const history = useHistory();
     
-    return (<div>
-            <h3>Success</h3>
-            <button onClick={()=> history.push('/')}>Leave more feedback</button>
-        </div>)
+    return <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+            <Grid item xs>
+            </Grid>
+            <Grid item xs={6}>
+                <Card 
+                    sx ={{ minWidth: 300, minHeight: 275}}
+                    style={{backgroundColor: "#e7ccaf"}}
+                    square>
+                    <CardContent sx={{minWidth: 200, minHeight: 175, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                        <h3>Success!</h3>
+                        <span>Your feedback was sent.</span><br />
+                        <span>When you're ready, take another survey.</span>
+                    </CardContent>
+                    <CardActions sx={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                        <Button onClick={()=> history.push('/')}>
+                            <CreateIcon style={{color:"#642e68"}} fontSize="large" />
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs>
+            </Grid>
+        </Grid>
+    </Box>
 }
     
 export default Success;
